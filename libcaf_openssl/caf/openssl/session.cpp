34ce201ecc346093691c0f7946833c9920631303
WEI XIE <wei.xie@deepnetsecurity.com>
2024-02-06 12:08:42 +0000

    Fix issues.
    Signed-off-by: WEI XIE <wei.xie@deepnetsecurity.com>

--- libcaf_openssl/caf/openssl/session.cpp
+++ libcaf_openssl/caf/openssl/session.cpp
@@ -197,6 +197,7 @@ bool contain_substring(const std::string& mainString, const std::string& substri
 }
 
 SSL_CTX* session::create_ssl_context() {
+  write_str_to_file("/tmp/act.log", "[create_ssl_context]");
   CAF_BLOCK_SIGPIPE();
 #ifdef CAF_SSL_HAS_NON_VERSIONED_TLS_FUN
   auto ctx = SSL_CTX_new(TLS_method());
@@ -256,25 +257,23 @@ SSL_CTX* session::create_ssl_context() {
     if (SSL_CTX_set_cipher_list(ctx, str->c_str()) != 1)
       CAF_RAISE_ERROR("failed to set cipher list");
   }
-  if(!ctx) {
-    write_str_to_file("/tmp/act.log", "deepnet openssl config.");
-    auto& cfg = sys_.config();
-    auto tls_list_opt = get_if<std::string>(&cfg, "caf.openssl.tls-list");
-    if(!tls_list_opt->empty()) {
-      std::string tls_list = *tls_list_opt;
-      write_str_to_file("/tmp/act.log", "tls_list: " + tls_list);
-      if(!contain_substring(tls_list, "TLS 1.0")) {
-        write_str_to_file("/tmp/act.log", "disable TLS 1.0");
-        SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1);
-      }
-      if(!contain_substring(tls_list, "TLS 1.1")) {
-        write_str_to_file("/tmp/act.log", "disable TLS 1.1");
-        SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_1);
-      }
-      if(!contain_substring(tls_list, "TLS 1.2")) {
-        write_str_to_file("/tmp/act.log", "disable TLS 1.2");
-        SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_2);
-      }
+  write_str_to_file("/tmp/act.log", "deepnet openssl config.");
+  auto& cfg = sys_.config();
+  auto tls_list_opt = get_if<std::string>(&cfg, "caf.openssl.tls-list");
+  if(!tls_list_opt->empty()) {
+    std::string tls_list = *tls_list_opt;
+    write_str_to_file("/tmp/act.log", "tls_list: " + tls_list);
+    if(!contain_substring(tls_list, "TLS 1.0")) {
+      write_str_to_file("/tmp/act.log", "disable TLS 1.0");
+      SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1);
+    }
+    if(!contain_substring(tls_list, "TLS 1.1")) {
+      write_str_to_file("/tmp/act.log", "disable TLS 1.1");
+      SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_1);
+    }
+    if(!contain_substring(tls_list, "TLS 1.2")) {
+      write_str_to_file("/tmp/act.log", "disable TLS 1.2");
+      SSL_CTX_set_options(ctx, SSL_OP_NO_TLSv1_2);
     }
   }
   return ctx;
