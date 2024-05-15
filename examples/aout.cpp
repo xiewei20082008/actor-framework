e5989c32436b0f8cdf80982212c056e2903238e1
WEI XIE <wei.xie@deepnetsecurity.com>
2024-05-16 00:02:42 +0100

    Add test example.
    Signed-off-by: WEI XIE <wei.xie@deepnetsecurity.com>

--- examples/aout.cpp
+++ examples/aout.cpp
@@ -5,29 +5,37 @@
 
 #include "caf/actor_ostream.hpp"
 #include "caf/actor_system.hpp"
+#include "caf/behavior.hpp"
 #include "caf/caf_main.hpp"
 #include "caf/event_based_actor.hpp"
+#include "caf/io/middleman.hpp"
+#include "caf/openssl/all.hpp"
+#include "caf/io/all.hpp"
+#include "caf/openssl/manager.hpp"
 
 using namespace caf;
 
-behavior printer(event_based_actor* self, int32_t num, int32_t delay) {
-  aout(self) << "Hi there! This is actor nr. " << num << "!" << std::endl;
-  std::chrono::milliseconds timeout{delay};
-  self->delayed_send(self, timeout, timeout_atom_v);
+struct dual_common_config : actor_system_config {
+    dual_common_config() {
+        load<caf::io::middleman>();
+        load<caf::openssl::manager>();
+        // set("caf.scheduler.policy", "sharing");
+        set("caf.openssl.certificate", "server.crt");
+        set("caf.openssl.key", "private.key");
+    }
+};
+
+behavior test_impl(event_based_actor *self) {
   return {
-    [=](timeout_atom) {
-      aout(self) << "Actor nr. " << num << " says goodbye after waiting for "
-                 << delay << "ms!" << std::endl;
-    },
+    [](int x) {
+      return x+1;
+    }
   };
 }
 
-void caf_main(actor_system& sys) {
-  std::random_device rd;
-  std::minstd_rand re(rd());
-  std::uniform_int_distribution<int32_t> dis{1, 99};
-  for (int32_t i = 1; i <= 50; ++i)
-    sys.spawn(printer, i, dis(re));
+void caf_main(actor_system& sys, const dual_common_config& cfg) {
+  auto a = sys.spawn(test_impl);
+  openssl::publish(a, 13999);
 }
 
-CAF_MAIN()
+CAF_MAIN(io::middleman, openssl::manager)
